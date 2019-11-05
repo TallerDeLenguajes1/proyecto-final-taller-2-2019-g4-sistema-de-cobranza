277f82fbe10dcf426cc50bcace60100497a107f0
capitobenj <48660025+capitobenj@users.noreply.github.com>
2019-11-04 23:28:59 -0300

    Agregado extensión NLog, Biblio de clases Acceso a datos y Biblio de clases Entidades.

---
+++ SistemaCobranza/AccesoADatos/Conexion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using NLog;
+
+namespace AccesoADatos
+{
+    public class Conexion
+    {
+        MySqlConnection connection;
+        public Conexion()
+        {
+            string str;
+            str = "server=localhost;userid=root;database=";
+
+            try
+            {
+                connection = new MySqlConnection(str);
+                connection.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se conecto con la base de datos.");
+                Logger logger = LogManager.GetCurrentClassLogger();
+                logger.Error(ex, "No se pudo conectar con la base de datos.");
+            }
+        }
+        public void Close()
+        {
+            try
+            {
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se conecto con la base de datos.");
+                Logger logger = LogManager.GetCurrentClassLogger();
+                logger.Error(ex, "No se pudo desconectar con la base de datos (tal vez no estaba conectada en primer lugar?).");
+            }
+            
+        }
+
+    }
+}
