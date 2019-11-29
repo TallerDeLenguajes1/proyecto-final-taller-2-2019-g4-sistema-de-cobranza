df24b63911f952a713f80729745b0664fc6f8941
carabajal-facundo <48761578+carabajal-facundo@users.noreply.github.com>
2019-11-29 20:57:02 -0300

    Update ExportarExcel.cs
    modificacion de exportar a excel

--- SistemaCobranza/Helpers/ExportarExcel.cs
+++ SistemaCobranza/Helpers/ExportarExcel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Entidades;
 using System.IO;
+using Microsoft.Office.Interop.Excel;
 
 namespace Helpers
 {
@@ -17,24 +18,51 @@ namespace Helpers
         public static void ExportarRegistro(List<Registro> registros)
         {
             string ruta = "Registros" + DateTime.Now.ToString("d-M-yyy--H-mm-ss") + ".csv";
-            //crea y escrive un archivo .csv 
+            //crea y escrive un archivo Excel
             //en el cual cada archivo se identifica con la fecha y hora que fue creado
-            using (StreamWriter sw = new StreamWriter(ruta))
+
+            //using (StreamWriter sw = new StreamWriter(ruta))
+            //{
+            //    sw.Write("Empresa;");
+            //    sw.Write("Deudor;");
+            //    sw.Write("Monto;"); 
+            //    sw.Write("Telefono;");
+            //    sw.WriteLine("DNI;");
+            //    foreach (var item in registros)
+            //    {
+            //        sw.Write($"{item.Deuda.Empresa.Nombre};");
+            //        sw.Write($"{item.Deuda.Deudor.ApellidoNombre};");
+            //        sw.Write($"{item.Deuda.Monto};");
+            //        sw.Write($"{item.Deuda.Deudor.Telefono};");
+            //        sw.WriteLine($"{item.Deuda.Deudor.Dni};");
+            //    }
+            //}
+
+            Microsoft.Office.Interop.Excel.Application excel;
+            excel = new Microsoft.Office.Interop.Excel.Application();
+            excel.Application.Workbooks.Add(true);
+
+            excel.Cells.Width(6);
+            excel.Cells[1,1] = "Empresa";
+            excel.Cells[1, 2] = "Monto";
+            excel.Cells[1, 3] = "Deudor";
+            excel.Cells[1, 4] = "Telefono";
+            excel.Cells[1, 5] = "DNI";
+
+            int fila = 2;
+            foreach (var item in registros)
             {
-                sw.Write("Empresa;");
-                sw.Write("Deudor;");
-                sw.Write("Monto;"); 
-                sw.Write("Telefono;");
-                sw.WriteLine("DNI;");
-                foreach (var item in registros)
-                {
-                    sw.Write($"{item.Deuda.Empresa.Nombre};");
-                    sw.Write($"{item.Deuda.Deudor.ApellidoNombre};");
-                    sw.Write($"{item.Deuda.Monto};");
-                    sw.Write($"{item.Deuda.Deudor.Telefono};");
-                    sw.WriteLine($"{item.Deuda.Deudor.Dni};");
-                }
+                excel.Cells[fila, 1] = item.Deuda.Empresa.Nombre;
+                excel.Cells[fila, 2] = item.Deuda.Deudor.ApellidoNombre;
+                excel.Cells[fila, 3] = item.Deuda.Monto;
+                excel.Cells[fila, 4] = item.Deuda.Deudor.Telefono;
+                excel.Cells[fila, 5] = item.Deuda.Deudor.Dni;
+                fila++;
             }
+
+            excel.SaveWorkspace(ruta);
+
+
 
         }
 
