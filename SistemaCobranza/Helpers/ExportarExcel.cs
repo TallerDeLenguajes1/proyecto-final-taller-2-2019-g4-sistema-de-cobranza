faf71f6c8472e1b334d55d0d004470abd828e4f7
carabajal-facundo <48761578+carabajal-facundo@users.noreply.github.com>
2019-12-02 22:48:53 -0300

    Update ExportarExcel.cs
    modificacion de foncion de exportar a Excel

--- SistemaCobranza/Helpers/ExportarExcel.cs
+++ SistemaCobranza/Helpers/ExportarExcel.cs
@@ -17,33 +17,17 @@ namespace Helpers
         /// <param name="registros"></param>
         public static void ExportarRegistro(List<Registro> registros)
         {
-            string ruta = "Registros" + DateTime.Now.ToString("d-M-yyy--H-mm-ss") + ".csv";
-            //crea y escrive un archivo Excel
-            //en el cual cada archivo se identifica con la fecha y hora que fue creado
-
-            //using (StreamWriter sw = new StreamWriter(ruta))
-            //{
-            //    sw.Write("Empresa;");
-            //    sw.Write("Deudor;");
-            //    sw.Write("Monto;"); 
-            //    sw.Write("Telefono;");
-            //    sw.WriteLine("DNI;");
-            //    foreach (var item in registros)   
-            //    {
-            //        sw.Write($"{item.Deuda.Empresa.Nombre};");
-            //        sw.Write($"{item.Deuda.Deudor.ApellidoNombre};");
-            //        sw.Write($"{item.Deuda.Monto};");
-            //        sw.Write($"{item.Deuda.Deudor.Telefono};");
-            //        sw.WriteLine($"{item.Deuda.Deudor.Dni};");
-            //    }
-            //}
+            string ruta = "Registros" + DateTime.Now.ToString("d-M-yyy--H-mm-ss") + ".xls";
 
             Microsoft.Office.Interop.Excel.Application excel;
+            Microsoft.Office.Interop.Excel.Workbook libroexcel;
+            Microsoft.Office.Interop.Excel.Worksheet hojaexcel;
+            object misValue = System.Reflection.Missing.Value;
             excel = new Microsoft.Office.Interop.Excel.Application();
-            excel.Application.Workbooks.Add(true);
+            libroexcel = excel.Workbooks.Add(misValue);
+            hojaexcel = (Microsoft.Office.Interop.Excel.Worksheet)libroexcel.Worksheets.get_Item(1);
 
-            excel.Cells.Width(6);
-            excel.Cells[1,1] = "Empresa";
+            excel.Cells[1, 1] = "Empresa";
             excel.Cells[1, 2] = "Monto";
             excel.Cells[1, 3] = "Deudor";
             excel.Cells[1, 4] = "Telefono";
@@ -60,8 +44,8 @@ namespace Helpers
                 fila++;
             }
 
-            excel.SaveWorkspace(ruta);
-
+            libroexcel.SaveAs(ruta);
+            libroexcel.Close();
 
 
         }
