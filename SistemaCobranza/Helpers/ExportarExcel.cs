f74854403623b1e6b421536a81c22277612b3c6b
capitobenj <48660025+capitobenj@users.noreply.github.com>
2019-12-06 17:53:43 -0300

    Ultima version del exportar a excel.

--- SistemaCobranza/Helpers/ExportarExcel.cs
+++ SistemaCobranza/Helpers/ExportarExcel.cs
@@ -21,40 +21,50 @@ namespace Helpers
         {
             try
             {
-                string ruta = "Registros" + DateTime.Now.ToString("d-M-yyy--H-mm-ss") + ".xls";
+                string ruta = "Registros " + DateTime.Now.ToString("d-M-yyy--H-mm-ss");
+                Microsoft.Win32.SaveFileDialog FileName;
+                FileName = new Microsoft.Win32.SaveFileDialog();
+                FileName.AddExtension = true;
+                FileName.DefaultExt = "xlsx";
+                FileName.FileName = ruta;
+                if (FileName.ShowDialog() == true)
+                {   
+                    Application excel;
+                    Workbook libroexcel;
+                    Worksheet hojaexcel;
+                    object misValue = System.Reflection.Missing.Value;
+                    excel = new Application();
+                    libroexcel = excel.Workbooks.Add(misValue);
+                    hojaexcel = (Worksheet)libroexcel.Worksheets.get_Item(1);
 
-                Application excel;
-                Workbook libroexcel;
-                Worksheet hojaexcel;
-                object misValue = System.Reflection.Missing.Value;
-                excel = new Application();
-                libroexcel = excel.Workbooks.Add(misValue);
-                hojaexcel = (Worksheet)libroexcel.Worksheets.get_Item(1);
+                    excel.Cells[1, 1] = "Empresa";
+                    excel.Cells[1, 2] = "Monto";
+                    excel.Cells[1, 3] = "Deudor";
+                    excel.Cells[1, 4] = "Telefono";
+                    excel.Cells[1, 5] = "DNI";
+                    excel.Cells[1, 6] = "Resultado";
+                    excel.Cells[1, 7] = "Observación";
+                    excel.Cells[1, 8] = "Usuario";
+                    excel.Cells[1, 9] = "Fecha";
 
-                excel.Cells[1, 1] = "Empresa";
-                excel.Cells[1, 2] = "Monto";
-                excel.Cells[1, 3] = "Deudor";
-                excel.Cells[1, 4] = "Telefono";
-                excel.Cells[1, 5] = "DNI";
-                excel.Cells[1, 3] = "Resultado";
-                excel.Cells[1, 4] = "Observación";
-                excel.Cells[1, 5] = "Usuario";
-
-                int fila = 2;
-                foreach (var item in registros)
-                {
-                    excel.Cells[fila, 1] = item.Deuda.Empresa.Nombre;
-                    excel.Cells[fila, 2] = item.Deuda.Deudor.ApellidoNombre;
-                    excel.Cells[fila, 3] = item.Deuda.Monto;
-                    excel.Cells[fila, 4] = item.Deuda.Deudor.Telefono;
-                    excel.Cells[fila, 5] = item.Deuda.Deudor.Dni;
-                    excel.Cells[fila, 6] = item.Resultado;
-                    excel.Cells[fila, 7] = item.Observacion;
-                    excel.Cells[fila, 8] = item.Usuario;
-                    fila++;
+                    int fila = 2;
+                    foreach (var item in registros)
+                    {
+                        excel.Cells[fila, 1] = item.Deuda.Empresa.Nombre;
+                        excel.Cells[fila, 2] = item.Deuda.Deudor.ApellidoNombre;
+                        excel.Cells[fila, 3] = item.Deuda.Monto;
+                        excel.Cells[fila, 4] = item.Deuda.Deudor.Telefono;
+                        excel.Cells[fila, 5] = item.Deuda.Deudor.Dni;
+                        excel.Cells[fila, 6] = item.Resultado;
+                        excel.Cells[fila, 7] = item.Observacion;
+                        excel.Cells[fila, 8] = item.Usuario.Nombre;
+                        excel.Cells[fila, 9] = item.FechaHora;
+                        fila++;
+                    }
+                    libroexcel.SaveAs(FileName.FileName);
+                    libroexcel.Close();
+                    excel.Quit();
                 }
-                libroexcel.Save();
-                libroexcel.Close();
             }
             catch (Exception ex)
             {
